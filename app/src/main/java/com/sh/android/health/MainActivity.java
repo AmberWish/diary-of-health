8cce730bc7d2b382ac04e58406bc00400037346d
o.khudyk <sh17prog@gmail.com>
2017-08-20 22:58:14 +0300

    main and nutrition activities

---
+++ app/src/main/java/com/sh/android/health/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sh.android.health;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button nutrition = (Button)findViewById(R.id.nutrition);
+        nutrition.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+    Intent intent = new Intent(this, NutritionActivity.class);
+    startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/sh/android/health/NutritionActivity.java
@@ -0,0 +1,13 @@
+package com.sh.android.health;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NutritionActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nutrition_layout);
+    }
+}
